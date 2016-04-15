e4744dcc237d496b2995d3530e4a7d580f7161c3
MATE <MATE@MATE-PC>
2016-04-15 12:09:02 +0200

    INICIO2

--- src/boletin_23/Ventana.java
+++ src/boletin_23/Ventana.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin_23;
 
 import java.awt.Color;
@@ -17,43 +12,38 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-
-
 public class Ventana {
-    public void MetodoPorQueSi(){
+    public void MetodoBOLETO(){
     JPanel panel = new JPanel();
     JPanel panel2 = new JPanel();
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame();           
     
-   
-    
-    
-    String[] strings = { "elemento1", "elemento2", "elemento3"};
+    String[] strings = { "objeto1", "objeto2", "objeto3"};
     JList lista = new JList(strings);
-    JButton boton3 = new JButton("boton");
-    JTextArea area = new JTextArea("area de texto");
+    JButton boton3 = new JButton("botonera");
+    JTextArea area = new JTextArea("area de escritura");
     
     //panel.setLayout(new GridLayout(3,2));
-    JLabel etiqueta = new JLabel("nome          ");
-    JLabel etiqueta2 = new JLabel("password");
-    JTextField nome = new JTextField(6);
-    JPasswordField contrasinal = new JPasswordField(6);
-    JButton boton1 = new JButton("premer");
-    JButton boton2 = new JButton("limpar");
+    JLabel etiqueta = new JLabel("nombre     ");
+    JLabel etiqueta2 = new JLabel("contraseña");
+    JTextField nombre = new JTextField(6);
+    JPasswordField contraseña = new JPasswordField(6);
+    JButton botonera1 = new JButton("clickea");
+    JButton botonera2 = new JButton("limpiar");
     FlowLayout dis = new FlowLayout(FlowLayout.CENTER,3,2);
     dis.setHgap(100);
     dis.setVgap(100);
     
     panel.setBounds(0,0,400,400);
-    panel.setBackground(Color.red);
+    panel.setBackground(Color.white);
     panel.add(etiqueta);
-    panel.add(nome);
+    panel.add(nombre);
     panel.add(etiqueta2);
-    panel.add(contrasinal);
-    panel.add(boton1);
-    panel.add(boton2);
+    panel.add(contraseña);
+    panel.add(botonera1);
+    panel.add(botonera2);
     
-    panel2.setBackground(Color.green);
+    panel2.setBackground(Color.blue);
     panel2.setBounds(0,450,400,400);
     panel2.add(lista);
     panel2.add(boton3);
@@ -63,9 +53,7 @@ public class Ventana {
     
     frame.setSize(400,800);
     frame.add(panel2);
-    frame.add(panel);
-    
-    
+    frame.add(panel);       
     
     frame.setLayout(null);
     frame.setVisible(true);
